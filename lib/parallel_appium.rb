3594f3cea47f1df9a0f4884e862a165e5017b54f
Javon Davis <jdavis@qualityworkscg.com>
2018-06-05 06:42:33 -0500

    resolving size for simple test

--- lib/parallel_appium.rb
+++ lib/parallel_appium.rb
@@ -6,7 +6,8 @@ module ParallelAppium
   # Setting up the selenium grid server
   class SeleniumGrid
 
-    def get_devices(platform)
+    def self.get_devices(platform)
+      ENV['THREADS'] = '1' if ENV['THREADS'].nil?
       if platform == 'android'
         Android.devices
       elsif platform == 'ios'
@@ -17,20 +18,24 @@ module ParallelAppium
 
   # Connecting to iOS devices
   class IOS
-    def self.simulators
-      `instruments -s devices`.split("\n").reverse # Reverse to use latest devices first
+    def self.detect_simulators
+      n = ENV['THREADS'].to_i
+      puts "N: #{n}"
+      `instruments -s devices`.split("\n").reverse[0, n] # Reverse to use latest devices first
     end
 
     def self.simulator_information
       re = /\([0-9]+\.[0-9]\) \[[0-9A-Z-]+\]/m
 
       # Filter out simulator info for iPhone platform version and udid
-      simulators.select { |simulator_data| simulator_data.include?('iPhone') && !simulator_data.include?('Apple Watch')}
-                .map { |simulator_data| simulator_data.scan(re)[0].tr('()[]', '').split }
+      simulators = detect_simulators
+      simulators.select { |simulator_data| simulator_data.include?('iPhone') && !simulator_data.include?('Apple Watch') }
+          .map { |simulator_data| simulator_data.scan(re)[0].tr('()[]', '').split }
     end
 
     def self.devices
       devices = []
+      simulators = detect_simulators
       simulator_information.each_with_index do |data, i|
         devices.push(name: simulators[i][0, simulators[i].index('(') - 1], platform: 'ios', os: data[0], udid: data[1],
                      wdaPort: 8100 + i + ENV['THREADS'].to_i, thread: i + 1)
@@ -44,17 +49,17 @@ module ParallelAppium
   # Connecting to Android devices
   class Android
     def start_emulators
-      emulators = (`emulator -list-avds`).split("\n")
-      Parallel.map(emulators, :in_threads=> emulators.size) do |emulator|
-        spawn("emulator -avd #{emulator} -scale 100dpi -no-boot-anim -no-audio -accel on &", :out=> "/dev/null")
+      emulators = `emulator -list-avds`.split("\n")
+      Parallel.map(emulators, in_threads: emulators.size) do |emulator|
+        spawn("emulator -avd #{emulator} -scale 100dpi -no-boot-anim -no-audio -accel on &", out: '/dev/null')
       end
     end
 
     def devices
       start_emulators
 
-      devs = `adb devices`.split("\n").select {|x| x.include? "\tdevice"}.map.each_with_index {|d, i| {platform: 'android', name: 'android', udid: d.split("\t")[0], wdaPort: 8100 + i, thread: i + 1}}
-      devices = devs.map {|x| x.merge(get_android_device_data(x[:udid]))}
+      devices = `adb devices`.split("\n").select { |x| x.include? "\tdevice" }.map.each_with_index { |d, i| {platform: 'android', name: 'android', udid: d.split("\t")[0], wdaPort: 8100 + i, thread: i + 1} }
+      devices = devices.map { |x| x.merge(get_android_device_data(x[:udid])) }
 
       ENV['DEVICES'] = JSON.generate(devices)
       puts 'Android devices'
--- spec/parallel_appium_spec.rb
+++ spec/parallel_appium_spec.rb
@@ -3,7 +3,13 @@ RSpec.describe ParallelAppium do
     expect(ParallelAppium::VERSION).not_to be nil
   end
 
-  it 'get\'s possible iOS devices' do
-    expect(ParallelAppium::SeleniumGrid.ios_devices).not_to be nil
+  it 'get\'s default number of iOS devices' do
+    expect(ParallelAppium::SeleniumGrid.get_devices('ios').size).to equal 1
+  end
+
+  it 'get\'s specified number of possible iOS devices' do
+    ENV['THREADS'] = '3'
+    expect(ParallelAppium::SeleniumGrid.get_devices('ios').size).to equal 3
+    ENV['THREADS'] = nil
   end
 end
